b635f194877b32885ea200a4fe464cb2a17f2774
Altamish Mahomed <gambit318@gmail.com>
2021-07-25 00:22:06 +0200

    Updated AssemblyInfo

--- ALsSoundSwitcher_Frontend/ALsSoundSwitcher/Properties/AssemblyInfo.cs
+++ ALsSoundSwitcher_Frontend/ALsSoundSwitcher/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ALsSoundSwitcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple tool for easily switching your default audio device. More info: https://github.com/creepyLANguy/SoundSwitcher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Altamish Mahomed")]
 [assembly: AssemblyProduct("ALsSoundSwitcher")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Altamish Mahomed, Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.7")]
+[assembly: AssemblyFileVersion("1.0.7")]
