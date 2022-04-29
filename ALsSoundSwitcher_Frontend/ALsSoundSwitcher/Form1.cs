a21d0ae59a3a19b23864732caf0cbf61a54f33e6
Altamish Mahomed <gambit318@gmail.com>
2022-04-29 14:07:58 +0200

    Add ability to robustly use closest matching named icon. Upped version.

--- ALsSoundSwitcher_Frontend/ALsSoundSwitcher/Form1.cs
+++ ALsSoundSwitcher_Frontend/ALsSoundSwitcher/Form1.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using ALsSoundSwitcher.Properties;
 
 namespace ALsSoundSwitcher
 {
@@ -30,6 +31,8 @@ namespace ALsSoundSwitcher
     private MenuItem menuItemRestart;
 
     private string activeMarker = " *";
+
+    private int bestIconNameMatchPercentageMinimum = 85;
 
     public Form1()
     {
@@ -312,15 +315,93 @@ namespace ALsSoundSwitcher
 
     private void SetIcon(string iconName)
     {
-      iconName = iconName.Trim();
-      var icon = ResourceExtended.GetIconByRawName(iconName);
-      if (icon == null)
+      notifyIcon1.Icon = Properties.Resources.Icon;
+
+      var alIcons = GetAllIconsInFolder();
+      if (alIcons.Count == 0)
       {
-        icon = Properties.Resources.Icon;
+        return;
       }
 
-      notifyIcon1.Icon = icon;
+      var matches = GetMatchPercentages(iconName.Trim(), alIcons);
+      var bestMatch = matches.OrderByDescending(it => it.Item2).First();
+      if (bestMatch.Item2 < bestIconNameMatchPercentageMinimum)
+      {
+        return;
+      }
+      notifyIcon1.Icon = ResourceExtended.GetIconByRawName(bestMatch.Item1);
     }
+
+    private List<string> GetAllIconsInFolder()
+    { 
+      var allIconFilePaths = Directory.GetFiles(Directory.GetCurrentDirectory(), "*", SearchOption.AllDirectories)
+          .Where(it => it.Contains(".ico")).ToList();
+      var allIcons = new List<string>(allIconFilePaths.Count); 
+      allIconFilePaths.ForEach(it => allIcons.Add(Path.GetFileName(it)));
+      return allIcons;
+    }
+
+    private List<Tuple<string, double>> GetMatchPercentages(string reference, List<string> candidates)
+    {
+      var list = new List<Tuple<string, double>>();
+
+      foreach (var candidate in candidates)
+      {
+        var matchPercentage = GetMatchPercentage(reference, candidate);
+        list.Add(new Tuple<string, double>(candidate, matchPercentage));
+      }
+
+      return list;
+    }
+
+    private double GetMatchPercentage(string reference, string candidate)
+    {
+      string largerString = reference;
+      if (candidate.Length > reference.Length)
+      {
+        largerString = candidate;
+      }
+
+      var editDistance = CalculateLevenshteinDistance(reference, candidate);
+
+      return ((double)(largerString.Length - editDistance) / largerString.Length) * 100;
+    }
+
+    //Source: https://gist.github.com/Davidblkx/e12ab0bb2aff7fd8072632b396538560#file-levenshteindistance-cs
+    private static int CalculateLevenshteinDistance(string source1, string source2) //O(n*m)
+    {
+      var source1Length = source1.Length;
+      var source2Length = source2.Length;
+
+      var matrix = new int[source1Length + 1, source2Length + 1];
+
+      // First calculation, if one entry is empty return full length
+      if (source1Length == 0)
+        return source2Length;
+
+      if (source2Length == 0)
+        return source1Length;
+
+      // Initialization of matrix with row size source1Length and columns size source2Length
+      for (var i = 0; i <= source1Length; matrix[i, 0] = i++) { }
+      for (var j = 0; j <= source2Length; matrix[0, j] = j++) { }
+
+      // Calculate rows and collumns distances
+      for (var i = 1; i <= source1Length; i++)
+      {
+        for (var j = 1; j <= source2Length; j++)
+        {
+          var cost = (source2[j - 1] == source1[i - 1]) ? 0 : 1;
+
+          matrix[i, j] = Math.Min(
+            Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+            matrix[i - 1, j - 1] + cost);
+        }
+      }
+      // return result
+      return matrix[source1Length, source2Length];
+    }
+
   }
 
   public class ResourceExtended
