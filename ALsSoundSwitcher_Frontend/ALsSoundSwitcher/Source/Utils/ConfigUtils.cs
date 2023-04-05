e135d3561b8f5e776657d50a02e0f489d473ac40
Altamish Mahomed <gambit318@gmail.com>
2023-04-05 21:27:48 +0200

    Completed implementation of JSON config.

--- ALsSoundSwitcher_Frontend/ALsSoundSwitcher/Source/Utils/ConfigUtils.cs
+++ ALsSoundSwitcher_Frontend/ALsSoundSwitcher/Source/Utils/ConfigUtils.cs
@@ -9,136 +9,60 @@ namespace ALsSoundSwitcher
 {
   public class Config
   {
-    public enum ConfigType
+    public static bool Read()
     {
-      Ini,
-      Json
-    }
-
-    public static bool Read(ConfigType configType = ConfigType.Json)
-    {
-      switch (configType)
-      {
-        case ConfigType.Ini:
-          return Read_INI();
-        case ConfigType.Json:
-          //AL.
-          //return Read_JSON();
-          return Read_INI();
-        default:
-          return false;
-      }
-    }
-    
-    public static void Save(ConfigType configType = ConfigType.Json)
-    {
-      switch (configType)
-      {
-        case ConfigType.Ini:
-          Save_INI();
-          break;
-        case ConfigType.Json:
-          Save_JSON();
-          break;
-      }
-    }
-
-    private static bool Read_INI()
-    { 
-      var dictionary = new Dictionary<string, string>();
-
       try
       {
-        var items = File.ReadAllText(ConfigFile).Split(new[] {"\r\n", "\r", "\n"}, StringSplitOptions.None);
-        foreach (var item in items)
-        {
-          if (item.Length == 0)
-          {
-            continue;
-          }
-
-          var indexOfDelim = item.IndexOf(ConfigDelimiter, StringComparison.Ordinal);
-          var key = item.Substring(0, indexOfDelim).Trim().ToLower();
-          var val = item.Substring(indexOfDelim + 1).Trim();
-          dictionary.Add(key, val);
-        }
-
-        string buff;
-
-        if (TryGetValue(Settings.Keys.BalloonTime, out buff))
-        {
-          Settings.Current.BalloonTime = Convert.ToInt32(buff);
-        }
-
-        if (TryGetValue(Settings.Keys.BestNameMatchPercentageMinimum, out buff))
-        {
-          Settings.Current.BestNameMatchPercentageMinimum = Convert.ToInt32(buff);
-        }
-
-        if (TryGetValue(Settings.Keys.DarkMode, out buff))
-        {
-          Settings.Current.DarkMode = Convert.ToInt32(buff);
-        }
-
-        if (TryGetValue(Settings.Keys.DefaultIcon, out buff))
-        {
-          if (buff.Length > 0)
-          {
-            Settings.Current.DefaultIcon = buff;
-          }
-        }
+        return ProcessJsonSettings();
       }
       catch (Exception)
       {
+        Save();
         return false;
       }
-
-      return true;
-
-      bool TryGetValue(string key, out string buff)
-      {
-        return dictionary.TryGetValue(key.ToLower(), out buff);
-      }
     }
 
-    private static void Save_INI()
+    private static bool ProcessJsonSettings()
     {
-      using (var sw = File.CreateText(ConfigFile))
-      {
-        foreach (var setting in typeof(Settings.SettingsStruct).GetFields())
-        {
-          var buff = setting.Name + ConfigDelimiter + setting.GetValue(Settings.Current);
-          sw.WriteLine(buff);
-        }
-      }
-    }
-
-    //AL.
-    //TODO - implement
-    private static bool Read_JSON()
-    {
-      var jsonString = File.ReadAllText("settings.json");
+      var jsonString = File.ReadAllText(ConfigFile);
       var jsonObject = JObject.Parse(jsonString);
       var jsonDict = jsonObject.ToObject<Dictionary<string, string>>();
 
-      string balloonTime = jsonDict["BalloonTime"];
-      string defaultIcon = jsonDict["DefaultIcon"];
+      if (jsonDict == null)
+      {
+        throw new Exception();
+      }
 
-      return false;
+      string buff;
+
+      if (jsonDict.TryGetValue(Settings.Keys.BalloonTime, out buff))
+      {
+        Settings.Current.BalloonTime = Convert.ToInt32(buff);
+      }
+
+      if (jsonDict.TryGetValue(Settings.Keys.BestNameMatchPercentageMinimum, out buff))
+      {
+        Settings.Current.BestNameMatchPercentageMinimum = Convert.ToInt32(buff);
+      }
+
+      if (jsonDict.TryGetValue(Settings.Keys.DarkMode, out buff))
+      {
+        Settings.Current.DarkMode = Convert.ToInt32(buff);
+      }
+
+      if (jsonDict.TryGetValue(Settings.Keys.DefaultIcon, out buff))
+      {
+        if (buff.Length > 0)
+        {
+          Settings.Current.DefaultIcon = buff;
+        }
+      }
+
+      return true;
     }
-
-    //AL.
-    //TODO - implement
-    private static void Save_JSON()
+    
+    public static void Save()
     {
-      //var inputList = new List<string>
-      //{
-      //  "BalloonTime=1500",
-      //  "BestNameMatchPercentageMinimum=25",
-      //  "DarkMode=1",
-      //  "DefaultIcon="
-      //};
-
       var jsonDict = new Dictionary<string, object>();
 
       foreach (var setting in typeof(Settings.SettingsStruct).GetFields())
