3228cd7641fea8ad21d96513def449a389205cd2
Altamish Mahomed <gambit318@gmail.com>
2023-12-05 12:06:10 +0200

    Prompt user to restart app when config change detected.

---
+++ ALsSoundSwitcher_Frontend/ALsSoundSwitcher/Source/Utils/FileWatcher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using ALsSoundSwitcher.Properties;
+
+namespace ALsSoundSwitcher
+{
+  public static class FileWatcher
+  {
+    public static void Run()
+    {
+      var fileWatcher = new FileSystemWatcher(Application.StartupPath);
+      fileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+      fileWatcher.Changed += OnChanged;
+      fileWatcher.Filter = Globals.ConfigFile;
+      fileWatcher.EnableRaisingEvents = true;
+    }
+
+    private static void OnChanged(object sender, FileSystemEventArgs e)
+    {
+      if (e.ChangeType != WatcherChangeTypes.Changed)
+      {
+        return;
+      }
+
+      try
+      {
+        TryHandlingFileUpdateEvent(e);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex);
+      }
+    }
+
+    private static void TryHandlingFileUpdateEvent(FileSystemEventArgs e)
+    {
+      var hash = File.ReadAllText(e.FullPath).GetHashCode();
+      if (hash == Globals.SettingsHash)
+      {
+        return;
+      }
+
+      Globals.SettingsHash = hash;
+
+      var selection =
+        MessageBox.Show(
+          Resources.FileWatcher_OnChanged,
+          Resources.ALs_Sound_Switcher,
+          MessageBoxButtons.YesNo,
+          MessageBoxIcon.Question
+        );
+
+      if (selection == DialogResult.Yes)
+      {
+        Application.Restart();
+      }
+    }
+  }
+}
