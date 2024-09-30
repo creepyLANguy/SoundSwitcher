73a171837d925beaddfd9cc4405babcdcda8cd16
MehdiDev <46635765+MehdiMamas@users.noreply.github.com>
2024-09-29 21:10:24 -0500

    Add SetCommunicationPlaybackDevice

--- SetPlaybackDevice/SetPlaybackDevice/Device.hpp
+++ SetPlaybackDevice/SetPlaybackDevice/Device.hpp
@@ -22,3 +22,29 @@ HRESULT SetAudioPlaybackDevice(const LPCWSTR devID)
 
 		return hr;
 }
+
+HRESULT SetCommunicationPlaybackDevice(const LPCWSTR devID)
+{
+    HRESULT hr;
+
+    // Initialize COM library
+    hr = CoInitialize(nullptr);
+
+    IPolicyConfigVista* pPolicyConfig;
+
+    // Set the role to eCommunications to specify this as the default communication device
+    const ERole role = eCommunications;
+
+    hr = CoCreateInstance(__uuidof(CPolicyConfigVistaClient), nullptr, CLSCTX_ALL, __uuidof(IPolicyConfigVista), reinterpret_cast<LPVOID*>(&pPolicyConfig));
+
+    if (SUCCEEDED(hr))
+    {
+        // Set the default communication device using the provided device ID
+        hr = pPolicyConfig->SetDefaultEndpoint(devID, role);
+        pPolicyConfig->Release();
+    }
+
+    CoUninitialize();
+
+    return hr;
+}
